f0c0eef357abccbedafbd0f46d3346e1eeb72682
Camilo <lucas.camilo@harmonit.com.br>
2024-12-03 15:38:43 -0300

    Criando a classe de contexto do EF Core

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb12dbebab73775c4ff9e330ebfad190f2fb8c84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0cbb7189ed10e1a6436449bf529bdbf238d978f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
