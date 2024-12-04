2b2e9fe67d176eb856785520d3f655426a5d0c68
Camilo <lucas.camilo@harmonit.com.br>
2024-12-04 12:33:23 -0300

    Criando o controlador de Produtos e fazendo a injecao de dependencia

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+015582316635cc238fb8529a0816ba090ae78773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502bfedb61864ca8c92e06ce0cc3785df91c2db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
