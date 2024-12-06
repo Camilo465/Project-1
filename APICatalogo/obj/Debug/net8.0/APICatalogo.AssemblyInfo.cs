100bad9b02aa1b8894711eb203cc4d5f6ed582ba
Camilo <lucas.camilo@harmonit.com.br>
2024-12-06 13:38:35 -0300

    Implementando filtros personalizados, no console

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5ad8a3feb6b0f2395236311a0d6a3de5d0ae00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f63bf0ef9d9496cd59e5daf69ea93d3c1977e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
